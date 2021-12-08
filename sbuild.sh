9e7e9333127f5078596e47324c1c0387ea4a0f88
Tómas Ingi <tomasingi00@gmail.com>
2021-12-08 19:51:32 +0000

    Fix permissions

--- sbuild.sh
+++ sbuild.sh
@@ -6,4 +6,9 @@
 # Directory of this script
 WD="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-cp $WD/usr-bin/tilix-startup /usr/bin/tilix-startup
+# sudo scripts
+sudo cp $WD/usr-bin/tilix-startup /usr/bin/tilix-startup
+sudo chmod +x /usr/bin/tilix-startup
+
+# run build.sh
+~/Linux-settings/build.sh
