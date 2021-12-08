e16aad7f9304ae4b016379c9d6a263790ff5a98b
Tómas Ingi <tomasingi00@gmail.com>
2021-12-08 04:28:18 +0000

    bashrc + improved build script

--- build.sh
+++ build.sh
@@ -2,5 +2,11 @@
 
 # Build script for some of the config files
 
-cp ./.vimrc ~/.vimrc
-cp ./vscode/settings.json ~/.config/Code/User/settings.json
+# Directory of this script
+WD="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+cp $WD/bashrc ~/.bashrc
+cp -TR $WD/skriftur ~/.skriftur
+
+cp $WD/vimrc ~/.vimrc
+cp $WD/vscode/settings.json ~/.config/Code/User/settings.json
